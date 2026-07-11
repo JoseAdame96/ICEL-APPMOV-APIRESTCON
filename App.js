bae94e68018ed2b54cf6ec03653e631b45d8949d
Jose Maria Adame Cruz <jose.mar.9607@gmail.com>
2026-07-10 18:41:45 -0600

    ADD - Inicializacion de proyecto en repositorio

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
+// ADAME CRUZ JOSE MARIA
+// 000087493
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// IMPORTACIONES NECESARIAS
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import ItemScreen from './screens/ItemsScreen';
+import DetailScreen from './screens/DetailScreen';
+import AddItemScreen from './screens/AddItemScreen';
+
+const Stack = createNativeStackNavigator();
+const initialItems = [
+  {
+    id:'1',
+    title:'Tarea 1',
+    description:'Crear app usando npx'
+  },
+  {
+    id:'2',
+    title:'Tarea 2',
+    description:'Correr app usando npx expo start'
+  }
+]
 export default function App() {
+  const[items, setItems] = useState(initialItems);
+
+  function addItem(newItem) {
+    setItems([...items], {
+      id: Date.now.toString,
+      title: newItem.title,
+      description: newItem.description
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/*Menu de navegacion*/}
+
+        <Stack.Screen name='Home'
+        component={HomeScreen}
+        options={{title:'Home'}}/>
+
+
+        <Stack.Screen name='Items'
+        options={{title:'Listado'}}
+        component={ItemScreen}/>
+
+
+        <Stack.Screen name='Detail'
+        component={DetailScreen}/>
+
+        <Stack.Screen name='AddItem'
+        options={{title:'Nuevo Elemento'}}>
+          { (props) => <AddItemScreen {...props} onAddItem={addItem}></AddItemScreen>}
+        </Stack.Screen>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
